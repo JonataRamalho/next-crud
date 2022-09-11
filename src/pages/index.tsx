d8d16eaa2bf645e30896c1b286c6f55d20fbbda7
Jonata Ramalho <jonata.ramalho@t10.digital>
2022-09-11 15:58:40 -0300

    feat: Cria layout e titulo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
     <div className={`
       flex h-screen justify-center items-center
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
     `}>
-      <span>Texto</span>
+      <Layout titulo="Cadastro Simples">
+        <span>Conteudo</span>
+      </Layout>
     </div>
   )
 }
